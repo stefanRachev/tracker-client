9d2564f2ed11f1c7363d85985e0d9ead5a667a73
stefanRachev <starfystef@gmail.com>
2025-02-25 19:06:47 +0200

    installing react-router-dom and creating a context and configuring it

---
+++ src/context/SessionContext.jsx
@@ -0,0 +1,24 @@
+// SessionContext.jsx
+import PropTypes from "prop-types";
+import { createContext, useState } from "react";
+
+export const SessionContext = createContext(null);
+
+
+
+export const SessionProvider = ({ children }) => {
+  const [session, setSession] = useState(null);
+
+  
+
+  return (
+    <SessionContext.Provider value={{ session, setSession }}>
+      {children}
+    </SessionContext.Provider>
+  );
+}; 
+
+SessionProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
---
+++ src/context/useSession.js
@@ -0,0 +1,6 @@
+import { useContext } from "react";
+import { SessionContext } from "./SessionContext";
+
+export const useSession = () => {
+    return useContext(SessionContext);
+  };
